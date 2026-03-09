fc0b01083b5622ee01bea473a6d7caef3709acd2
harshsksh <hr392002@gmail.com>
2026-03-09 14:24:20 +0530

    feat: update Home page layout and styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,14 @@ export default function Home() {
   console.log("Hello from the Home page!");
   return (
     <>
-    <h1 className="heading">Welcome to the Home Page</h1>
+
+    <section className="pink_container">
+      <h1 className="heading">pitch your startup , <br />connect with entrepreneurs</h1>
+
+      <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and get noticed in Virtual Competitions</p>
+    </section>
+
+    <h1 className="heading">Home Page</h1>
     
     </>
     
