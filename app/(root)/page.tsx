93adaf9806890013f0fa4cd9ee7fa1c7b87afe89
harshsksh <hr392002@gmail.com>
2026-03-09 10:42:17 +0530

    added tailwind global css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   console.log("Hello from the Home page!");
   return (
     <>
-    <h1 className="text-3xl">Welcome to the Home Page</h1>
+    <h1 className="heading">Welcome to the Home Page</h1>
     
     </>
     
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
       <nav className='flex justify-between items-center'>
         <Link href="/">
-          <Image src="/logo.png" alt="Logo" width={100} height={30} />
+          <Image src="/logo.png" alt="Logo" width={80} height={30} />
         </Link>
 
         <div className='flex items-center gap-5 text-black'>
