a4d51531940b3bf9bf26de9ac81698d043350c19
harshsksh <hr392002@gmail.com>
2026-03-10 10:55:16 +0530

    feat: implement StartupCard component and enhance Home page with search results display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
+import { Description } from "@radix-ui/react-toast";
 import SearchForm from "../../components/searchForm";
+import StartupCard from "../../components/StartupCard";
 
 export default async function Home({searchParams} : {
   searchParams : Promise<{ query?: string }>
 }) {
   const query  = (await searchParams).query;
+
+  const posts = [{
+    _createdAt :  new Date().toISOString(),
+    views : 55,
+    author : {_id : 1, name : "John Doe"},
+    _id : 1,
+    description : "This is a description of the post",
+    image : "https://source.unsplash.com/random/300x300/?technology",
+    title : "StartupVerse",
+    category : "Technology"
+  }]
+
   console.log("Hello from the Home page!");
   return (
     <>
@@ -14,6 +28,21 @@ export default async function Home({searchParams} : {
       <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and get noticed in Virtual Competitions</p>
 
       <SearchForm query={query} />
+    </section>
+
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query? `Search results for "${query}"` : "All Startups"}
+      </p>
+      <ul className="mt-7 card_grid text-black">
+        {posts?.length > 0 ? (
+          posts.map((post : StartupCardType, index : number) => (
+            <StartupCard key={post?._id} post={post}/>
+          ))
+        ) : (
+          <p className="no-results">No startups found.</p>
+        )}
+      </ul>
     </section>
 
     
